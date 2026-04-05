869e7a60f6023802225fa9d3e6be66de76a66718
phatngo <ntp18799@gmail.com>
2026-04-05 16:07:49 +0700

    Add mechanism to run tests by tags

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,25 @@
-node_modules/.bin/cucumber-js -p default
+#!/bin/bash
+
+source .env
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    --tags)
+        TAGS="$2"
+        shift # past argument=value
+        shift
+    ;;
+    *)
+        echo "usage:
+        --tags          Scenario tags"
+        exit 1
+    ;;
+esac
+done
+
+if [[ ${TAGS} ]]; then
+    node_modules/.bin/cucumber-js -p default -t "${TAGS}"
+else
+    node_modules/.bin/cucumber-js -p default
+fi
