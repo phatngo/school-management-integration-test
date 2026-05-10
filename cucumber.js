d33ecb113376df1b66645bdc238b43ad3d2b95ad
phatngo <ntp18799@gmail.com>
2026-05-10 20:29:13 +0700

    add htmp test report config

--- cucumber.js
+++ cucumber.js
@@ -2,6 +2,15 @@ module.exports = {
   default: {
     paths: ["features/**/*.feature"],
     require: ["step_definitions/**/*.ts"],
-    requireModule: ["ts-node/register"]
+    requireModule: ["ts-node/register"],
+    format: [
+      `html:test-report.html`,
+      "progress-bar",
+    ],
+    formatOptions: {
+      html: {
+        externalAttachments: true,
+      },
+    },
   },
 };
